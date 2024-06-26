16ad7c4f14214d94cabd2508a7f047da8f17c269
Yunus Emre AYHAN <ayhanyunusemre@gmail.com>
2024-06-26 15:38:30 +0300

    adding some comments

--- src/test/sample_shatranj_gamelay.cpp
+++ src/test/sample_shatranj_gamelay.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <limits.h>
+#include <utility>
 
 #include "position.h"
 #include "shatranj.h"
@@ -8,11 +9,18 @@
 namespace
 {
 
-void CheckPossibleMoves(
-    shatranj::Shatranj &shatranj,
-    const std::vector<std::pair<shatranj::Position, shatranj::Position>> &possiblemovesoflastcapturedpiece,
-    const std::vector<std::string> &expectedmoves)
+void CheckPossibleMoves(shatranj::Shatranj &shatranj, std::string piece_coordinates,
+                        const std::vector<std::string> &expectedmoves)
 {
+    auto lastcapturedplace =
+        shatranj.GetBoard()->GetPieces()->GetPiece(shatranj::Position(std::move(piece_coordinates)));
+    ASSERT_TRUE(lastcapturedplace.has_value());
+    if (!lastcapturedplace.has_value())
+    {
+        return;
+    }
+    auto lastcapturedpiece = *lastcapturedplace;
+    auto possiblemovesoflastcapturedpiece = lastcapturedpiece->GetPossibleMoves(shatranj.GetBoard());
     for (const auto &move : possiblemovesoflastcapturedpiece)
     {
         const std::string movestring = move.first.ToString() + move.second.ToString();
@@ -56,33 +64,26 @@ TEST(SampleCaptureTest_Piyade, Positive)
     }
     {
         {
+            // piyade can not capture in front check get possible moves only 2 crosses possible front filled
             shatranj::Shatranj shatranj(
                 std::make_shared<shatranj::Player>(std::string("player1"), shatranj::Color::kWhite),
                 std::make_shared<shatranj::Player>(std::string("player2"), shatranj::Color::kBlack));
             EXPECT_EQ(shatranj.PlaySeq({"a2a3", "b7b6", "b2b3", "b6b5", "c2c3", "b5b4", "d2d3"}), true);
-            auto lastcapturedplace = shatranj.GetBoard()->GetPieces()->GetPiece(shatranj::Position("b4"));
-            auto lastcapturedpiece = *lastcapturedplace;
-            auto possiblemovesoflastcapturedpiece = lastcapturedpiece->GetPossibleMoves(shatranj.GetBoard());
-            std::cout << *(shatranj.GetBoard()) << std::endl;
-            std::vector<std::string> expectedmoves = {"b4a3", "b4c3"};
-            CheckPossibleMoves(shatranj, possiblemovesoflastcapturedpiece, expectedmoves);
+            CheckPossibleMoves(shatranj, "b4", {"b4a3", "b4c3"});
             EXPECT_EQ(shatranj.PlaySeq({"b4a3"}), true);
         }
         {
+            // piyade can not capture in front check get possible moves all 3 possible, front empty
             shatranj::Shatranj shatranj(
                 std::make_shared<shatranj::Player>(std::string("player1"), shatranj::Color::kWhite),
                 std::make_shared<shatranj::Player>(std::string("player2"), shatranj::Color::kBlack));
             EXPECT_EQ(shatranj.PlaySeq({"a2a3", "b7b6", "e2e3", "b6b5", "c2c3", "b5b4", "d2d3"}), true);
-            auto lastcapturedplace = shatranj.GetBoard()->GetPieces()->GetPiece(shatranj::Position("b4"));
-            auto lastcapturedpiece = *lastcapturedplace;
-            auto possiblemovesoflastcapturedpiece = lastcapturedpiece->GetPossibleMoves(shatranj.GetBoard());
             std::cout << *(shatranj.GetBoard()) << std::endl;
-            std::vector<std::string> expectedmoves = {"b4a3", "b4c3", "b4b3"};
-            CheckPossibleMoves(shatranj, possiblemovesoflastcapturedpiece, expectedmoves);
+            CheckPossibleMoves(shatranj, "b4", {"b4a3", "b4c3", "b4b3"});
             EXPECT_EQ(shatranj.PlaySeq({"b4b3"}), true);
         }
         {
-
+            // piyade can not capture in front
             shatranj::Shatranj shatranj(
                 std::make_shared<shatranj::Player>(std::string("player1"), shatranj::Color::kWhite),
                 std::make_shared<shatranj::Player>(std::string("player2"), shatranj::Color::kBlack));
@@ -90,7 +91,7 @@ TEST(SampleCaptureTest_Piyade, Positive)
             EXPECT_EQ(shatranj.PlaySeq({"b4c3"}), true);
         }
         {
-
+            // piyade can not capture in front
             shatranj::Shatranj shatranj(
                 std::make_shared<shatranj::Player>(std::string("player1"), shatranj::Color::kWhite),
                 std::make_shared<shatranj::Player>(std::string("player2"), shatranj::Color::kBlack));
@@ -110,11 +111,28 @@ TEST(SampleCaptureTest_Rook, Positive)
     EXPECT_EQ(lastcapturedplace.has_value(), true);
     auto lastcapturedpiece = *lastcapturedplace;
     EXPECT_EQ(lastcapturedpiece->GetSymbol() == 'R', true);
-    auto possiblemovesoflastcapturedpiece = lastcapturedpiece->GetPossibleMoves(shatranj.GetBoard());
     EXPECT_TRUE(lastcapturedpiece->GetPlayer().lock()->GetColor() == shatranj::Color::kWhite);
-    std::vector<std::string> expectedmoves = {"a7a8", "a7a6", "a7a5", "a7a4", "a7a3", "a7a2", "a7a1", "a7b7", "a7c7"};
-    EXPECT_EQ(possiblemovesoflastcapturedpiece.size(), expectedmoves.size());
-    CheckPossibleMoves(shatranj, possiblemovesoflastcapturedpiece, expectedmoves);
+    CheckPossibleMoves(shatranj, "a7", {"a7a8", "a7a6", "a7a5", "a7a4", "a7a3", "a7a2", "a7a1", "a7b7", "a7c7"});
+}
+
+TEST(SampleCaptureTest_Horse, Positive)
+{
+    {
+        shatranj::Shatranj shatranj(
+            std::make_shared<shatranj::Player>(std::string("player1"), shatranj::Color::kWhite),
+            std::make_shared<shatranj::Player>(std::string("player2"), shatranj::Color::kBlack));
+        auto piece_horse_b1 = shatranj.GetBoard()->GetPieces()->GetPiece(shatranj::Position("b1"));
+        auto piece_horse_b8 = shatranj.GetBoard()->GetPieces()->GetPiece(shatranj::Position("b8"));
+        auto piece_horse_instance_b8 = *piece_horse_b1;
+        CheckPossibleMoves(shatranj, "b1", {"b1a3", "b1c3"});
+        EXPECT_EQ(shatranj.PlaySeq({"b1c3"}), true);
+        piece_horse_b1 = shatranj.GetBoard()->GetPieces()->GetPiece(shatranj::Position("b1"));
+        CheckPossibleMoves(shatranj, "c3", {"c3b5", "c3a4", "c3d5", "c3e4", "c3b1"});
+        EXPECT_EQ(shatranj.PlaySeq({"b8c5"}), false);
+        EXPECT_EQ(shatranj.PlaySeq({"b8c6"}), true);
+        piece_horse_b8 = shatranj.GetBoard()->GetPieces()->GetPiece(shatranj::Position("b8"));
+        CheckPossibleMoves(shatranj, "c6", {"c6b4", "c6a5", "c6e5", "c6d4", "c6b8"});
+    }
 }
 
 } // namespace
